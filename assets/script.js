0bccadd9fef64b1768f2cc24d89ab6f84e7bc41f
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-05-07 21:49:44 -0400

    developing for loops and if statements

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,20 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(){
   // Ask for length of password
   var passwordLength = window.prompt("Length of password");
+  var lowerCase = window.prompt("Do you want lowercase");
+  var upperCase = window.prompt("Do you want uppercase");
+  var numeric = window.prompt("Do you want numeric");
+  var specialCharacters = window.prompt("Do you want special characters");
   // at least 8 characters an no more then 128
-  if (passwordLength  )
+  if (passwordLength < 8 && passwordLength > 128) {
+    window.alert("Chose a password length from 8 characters to 128 characters");
+    return;
+  } 
+
+  
+
+
+
   // ask user do they want lowercase, uppercase, numeric, or special characters
   // Input from user should be validated an choose at one character type (IF statement)
   // randomize all available characters that the user selected. 
